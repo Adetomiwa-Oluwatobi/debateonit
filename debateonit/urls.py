269004c3ad8a00865193a5997b32358620fa371e
Adetomiwa-Oluwatobi <adetomiwaoluwatobi45@gmail.com>
2025-04-29 14:47:13 +0100

    Set rooms.landing_page as root homepage

--- debateonit/urls.py
+++ debateonit/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('debate/', include('rooms.urls')),  # Include the debate app URLs
+    path('', include('rooms.urls')),  # Include the debate app URLs
     
 ]
 
